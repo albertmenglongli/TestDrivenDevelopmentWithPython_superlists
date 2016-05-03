2403460217c254ba1e541f0d9cd2a087e3952771
limenglong <albert.menglongli@gmail.com>
2016-05-04 01:14:30 +0800

    Code format pep8

--- functional_tests.py
+++ functional_tests.py
@@ -2,9 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
+
     def setUp(self):
-        self.browser=webdriver.Firefox()
+        self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
@@ -13,31 +15,29 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        header_text=self.browser.find_element_by_tag_name('h1').text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # app want user to add a new todo
-        input_box=self.browser.find_element_by_id('id_new_item')
+        input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             input_box.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
-        
         # user type buy peacock fethers
         input_box.send_keys('Buy peacock feathers')
 
         # after user press Enter button, he could see the todo item in table
         input_box.send_keys(Keys.ENTER)
 
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_element_by_tag_name('tr')
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows)
         )
 
-
         self.fail('Finish the test')
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     unittest.main()
--- lists/views.py
+++ lists/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home_page(request):
     return HttpResponse('<html><title>To-Do</title></html>')
